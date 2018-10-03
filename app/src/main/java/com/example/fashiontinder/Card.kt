b819671f793d50f8a2f39dcb000347ce6efc33c9
Juan Fuentes <juan.fuentes@mango.com>
2018-10-03 09:34:19 +0200

    Labels Bug solved.

--- app/src/main/java/com/example/fashiontinder/Card.kt
+++
@@ -1,3 +0,0 @@
-package com.example.fashiontinder
-
-data class Card(val title: String, val subtitle: String, val url: String)
--- app/src/main/java/com/example/fashiontinder/CardInfoActivity.kt
+++ app/src/main/java/com/example/fashiontinder/CardInfoActivity.kt
@@ -15,44 +15,42 @@ class CardInfoActivity : AppCompatActivity() {
         setup(product)
     }
 
-    fun setup(product: Product){
-        var labelString: String
+    private fun setup(product: Product){
+        val listLabels = ArrayList<HashMap<String, Float>>()
+        product.label0?.let { listLabels.add(it) }
+        product.label1?.let { listLabels.add(it) }
+        product.label2?.let { listLabels.add(it) }
+        product.label3?.let { listLabels.add(it) }
+        product.label4?.let { listLabels.add(it) }
+        product.label5?.let { listLabels.add(it) }
+        product.label6?.let { listLabels.add(it) }
+        product.label7?.let { listLabels.add(it) }
+        product.label8?.let { listLabels.add(it) }
+        product.label9?.let { listLabels.add(it) }
 
-        val(label0Key, label0Value) = getLabel(product.label0)
-        val(label1Key, label1Value) = getLabel(product.label1)
-        val(label2Key, label2Value) = getLabel(product.label2)
-        val(label3Key, label3Value) = getLabel(product.label3)
-        val(label4Key, label4Value) = getLabel(product.label4)
-        val(label5Key, label5Value) = getLabel(product.label5)
-        val(label6Key, label6Value) = getLabel(product.label6)
-        val(label7Key, label7Value) = getLabel(product.label7)
-        val(label8Key, label8Value) = getLabel(product.label8)
-        val(label9Key, label9Value) = getLabel(product.label9)
+        var strLabels  = ""
+        listLabels.forEach{
+            val(labelKey, labelValue) = getLabel(it)
 
+            strLabels = "$strLabels\t$labelKey - $labelValue\n"
+        }
 
-        labelString = "Product id: " + product.id + "\n" +
-                "Product name: " + product.name + "\n" +
-                "Head Inclination: \n" +
-                    "\t Y: " + product.angleY + "\n" +
-                    "\t Z: " + product.angleZ + "\n" +
-                    "\t right eye: " + product.rightEye + "\n" +
-                    "\t left eye: " + product.leftEye + "\n" +
-                    "\t smile: " + product.smile + "\n" +
-                "Labels: \n" +
-                    "\t" + label0Key + " - " + label0Value + "\n" +
-                    "\t" + label1Key + " - " + label1Value + "\n" +
-                    "\t" + label2Key + " - " + label2Value + "\n" +
-                    "\t" + label3Key + " - " + label3Value + "\n" +
-                    "\t" + label4Key + " - " + label4Value + "\n" +
-                    "\t" + label5Key + " - " + label5Value + "\n" +
-                    "\t" + label6Key + " - " + label6Value + "\n" +
-                    "\t" + label7Key + " - " + label7Value + "\n" +
-                    "\t" + label8Key + " - " + label8Value + "\n" +
-                    "\t" + label9Key + " - " + label9Value + "\n"
+        val labelTextView:TextView = findViewById(R.id.product_id)
+        labelTextView.text = product.id
 
+        val nameTextView:TextView = findViewById(R.id.product_name)
+        nameTextView.text = product.name
 
-        val labelTextView:TextView = findViewById(R.id.product_labels)
-        labelTextView.text = labelString
+        val headTextView:TextView = findViewById(R.id.head)
+        val headStr = "\t Y: " + product.angleY + "\n" +
+                "\t Z: " + product.angleZ + "\n" +
+                "\t right eye: " + product.rightEye + "\n" +
+                "\t left eye: " + product.leftEye + "\n" +
+                "\t smile: " + product.smile + "\n"
+        headTextView.text = headStr
+
+        val labelsTextView:TextView = findViewById(R.id.labels)
+        labelsTextView.text = strLabels
     }
 
     fun getLabel(map: HashMap<String, Float>?): Pair<String, Float>{
@@ -65,5 +63,4 @@ class CardInfoActivity : AppCompatActivity() {
 
         return Pair(labelKey, keyValue)
     }
-
 }
